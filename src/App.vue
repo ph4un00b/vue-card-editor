544a90a3731c4cfbc954f8a3446118b0a7d62646
ph4un00b <1057021+ph4un00b@users.noreply.github.com>
2022-10-29 12:26:03 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script>
+import { defineComponent } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+})
+</script>
+
+<template>
+  <div id="app">
+    <img width="30%" src="@/assets/logo.svg" alt="Vite logo" />
+    <HelloWorld msg="Vue 2 + Vite" />
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
