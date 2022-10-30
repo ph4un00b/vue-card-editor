adf92d4e07ed49f69486d00081fb731113599769
carbon based lifeform <faunotv@gmail.com>
2022-10-30 03:29:06 -0600

    checkpoint

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,12 +4,22 @@ import { ref, defineComponent, h } from 'vue'
 export default defineComponent({
   name: 'HelloWorld',
   props: {
-    msg: {
+    posX: {
       type: String,
       required: !true,
+      default: '0%'
     },
+    blend: {
+      type: String,
+      required: !true,
+      default:'none'
+    },
+    filter: {
+      type: String,
+      required: true
+    }
   },
-  setup(_, {slots}) {
+  setup({posX, blend, filter}, {slots,}) {
     let string = ''
     let bgImage = ''
     console.log(slots.default ? slots.default() : [])
@@ -27,7 +37,7 @@ export default defineComponent({
        bgImage += item.componentOptions.propsData.bg + ", "
       })
 
-  console.log({bgImage})
+  console.log({bgImage, posX})
   const l1 = "repeating-linear-gradient(0deg, rgb(255, 119, 115) calc(5%*1), rgba(255, 237, 95, 1) calc(5%*2), rgba(168, 255, 95, 1) calc(5%*3), rgba(131, 255, 247, 1) calc(5%*4), rgba(120, 148, 255, 1) calc(5%*5), rgb(216, 117, 255) calc(5%*6), rgb(255, 119, 115) calc(5%*7))"
   const l2 = "repeating-linear-gradient(/* lever -> */ 135deg /* <-*/, #0e152e 0%, hsl(180, 10%, 60%) 3.8%, hsl(180, 29%, 66%) 4.5%, hsl(180, 10%, 60%) 5.2%, #0e152e 10%, #0e152e 12%)"
   const lradial ="radial-gradient(farthest-corner circle at 50% 50%, rgba(0, 0, 0, .1) 12%, rgba(0, 0, 0, .15) 20%, rgba(0, 0, 0, .25) 120%)"
@@ -37,18 +47,19 @@ export default defineComponent({
       style: `
       width: 60vw;
       max-width: 300px;
-      mix-blend-mode: color-dodge;
+      mix-blend-mode: ${blend};
       background-image: ${test};
       background-size: 50%, 200% 700%, 300%, 200%;
       background-blend-mode: exclusion, hue, hard-light, exclusion;
       background-position: 0% 50%, 50% 50%, 50% 50%;
-      filter: brightness(calc((0*0.3) + 0.5)) contrast(2) saturate(1.5);
+      filter: ${filter};
       aspect-ratio: 5/6;
       // border: 0.1rem solid;
       // border-color: red;
       margin: auto;
       position: absolute;
-      top: 0; left: 0; bottom: 0; right: 0;
+      transform: translateX(${posX});
+      top: 0; left: -0%; bottom: 0; right: 0;
       `
       })
   },
