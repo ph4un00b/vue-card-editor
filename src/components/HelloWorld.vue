772789155167d17f7f27545396eee2a166d2cb17
carbon based lifeform <faunotv@gmail.com>
2022-10-30 13:06:38 -0600

    checkpoint, compiling

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -20,8 +20,9 @@ export default defineComponent({
     }
   },
   setup({posX, blend, filter}, {slots,}) {
-    let string = ''
+    let blends = ''
     let bgImage = ''
+    let zooms = ''
     console.log(slots.default ? slots.default() : [])
     slots.default().forEach(item => {
       /**
@@ -33,12 +34,17 @@ export default defineComponent({
        * }
        */
       //  console.log(item.componentOptions.propsData)
-       string += item.componentOptions.propsData.blend + ", "
-       console.log(item.componentOptions.propsData.bg)
+       blends += item.componentOptions.propsData.blend + ", "
+      //  console.log(item.componentOptions.propsData.bg)
        bgImage += item.componentOptions.propsData.bg + ","
+       zooms += item.componentOptions.propsData.zoom + ","
       })
 
-  bgImage = bgImage.substring(0, bgImage.lastIndexOf(","))
+  const removeLastComma = (x) => x.substring(0, x.lastIndexOf(","))
+  bgImage = removeLastComma(bgImage)
+  blends = removeLastComma(blends)
+  zooms = removeLastComma(zooms)
+  console.log(zooms)
   const l1 = "repeating-linear-gradient(0deg, rgb(255, 119, 115) calc(5%*1), rgba(255, 237, 95, 1) calc(5%*2), rgba(168, 255, 95, 1) calc(5%*3), rgba(131, 255, 247, 1) calc(5%*4), rgba(120, 148, 255, 1) calc(5%*5), rgb(216, 117, 255) calc(5%*6), rgb(255, 119, 115) calc(5%*7))"
   const l2 = "repeating-linear-gradient(/* lever -> */ 135deg /* <-*/, #0e152e 0%, hsl(180, 10%, 60%) 3.8%, hsl(180, 29%, 66%) 4.5%, hsl(180, 10%, 60%) 5.2%, #0e152e 10%, #0e152e 12%)"
   const lradial ="radial-gradient(farthest-corner circle at 50% 50%, rgba(0, 0, 0, .1) 12%, rgba(0, 0, 0, .15) 20%, rgba(0, 0, 0, .25) 120%)"
@@ -50,17 +56,17 @@ export default defineComponent({
       max-width: 300px;
       mix-blend-mode: ${blend};
       background-image: ${bgImage.trim()};
-      background-size: 50%, 200% 700%, 300%, 200%;
-      background-blend-mode: exclusion, hue, hard-light, exclusion;
+      background-size: ${zooms};
+      background-blend-mode: ${blends};
       background-position: 0% 50%, 50% 50%, 50% 50%;
       filter: ${filter};
       aspect-ratio: 5/6;
-      border: 0.1rem solid;
-      border-color: red;
       margin: auto;
       position: absolute;
-      transform: translateX(${posX});
       top: 0; left: 0%; bottom: 0; right: 0;
+      border: /** debug */ 0.1rem solid;
+      border-color: /** debug */ red;
+      transform: /** debug */ translateX(${posX});
       `
       })
   },
