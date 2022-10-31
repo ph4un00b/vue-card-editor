551946b453a14d4e4188b28de816479df2eb475e
ph4u <1057021+ph4un00b@users.noreply.github.com>
2022-10-30 23:48:24 -0600

    chore(Root): chkpoint, main buffer backgrounds

--- src/components/Composition.vue
+++ src/components/Composition.vue
@@ -38,9 +38,10 @@ export default {
     // },
   },
   render(h, context) {
+    console.log(this)
     const slots = context.slots()
     let blendModes = ''
-    let buffers = ''
+    let buffers = []
     let bgSizes = ''
     let bgPositions = ''
     slots.default.forEach(
@@ -54,21 +55,22 @@ export default {
          * best API (componentOptions) to
          * fecth props data */
         blendModes += blend + ', '
-        buffers += bg + ','
+        buffers.push(bg)
         bgSizes += zoom + ','
         bgPositions += pos ? pos + ',' : '0% 50%,'
       }
     )
 
+    // console.log(buffers)
     const rmLastComma = (x) => x.substring(0, x.lastIndexOf(','))
-    buffers = rmLastComma(buffers)
+    // buffers = rmLastComma(buffers)
     blendModes = rmLastComma(blendModes)
     bgSizes = rmLastComma(bgSizes)
     bgPositions = rmLastComma(bgPositions)
     const props = context.props
-    const lastLayer = slots?.last
-    const children = lastLayer ? [lastLayer] : []
-
+    const isLastLayer = slots?.last
+    const children = isLastLayer ? [slots?.last] : []
+    // console.log(lastLayer?.map(v => console.log(v.render())))
     const mainBuffers = () => {
       const staticStyles = `
       width: 20vh;
@@ -76,18 +78,18 @@ export default {
       max-width: calc(768px / 4);
       // margin: auto;
       position: absolute;
-      border: /** debug */ 0.1rem solid;
-      border-color: /** debug */ red;
+      // border: /** debug */ 0.1rem solid;
+      // border-color: /** debug */ red;
       left: 0;
       `
       const tag = 'section'
-      return !lastLayer
+      return !isLastLayer
         ? []
         : [
-            h(tag, { style: `${staticStyles} top: 0;` }, 'buffer0'),
-            h(tag, { style: `${staticStyles} top: 20vh;` }, 'buffer1'),
-            h(tag, { style: `${staticStyles} top: 40vh;` }, 'buffer2'),
-            h(tag, { style: `${staticStyles} top: 60vh;` }, 'buffer3'),
+            h(tag, { style: `${staticStyles} background-image: ${buffers[0]}; top: 0;` }, 'buffer0'),
+            h(tag, { style: `${staticStyles} background-image: ${buffers[1]}; top: 20vh;` }, 'buffer1'),
+            h(tag, { style: `${staticStyles} background-image: ${buffers[2]}; top: 40vh;` }, 'buffer2'),
+            h(tag, { style: `${staticStyles} background-image: ${buffers[3]}; top: 60vh;` }, 'buffer3'),
           ]
     }
 
@@ -103,7 +105,7 @@ export default {
       right: 0;
       `
       const tag = 'section'
-      return !lastLayer
+      return !isLastLayer
         ? []
         : [
             h(tag, { style: `${staticStyles} top: 0;` }, 'buffer20'),
@@ -112,7 +114,7 @@ export default {
             h(tag, { style: `${staticStyles} top: 60vh;` }, 'buffer23'),
           ]
     }
-    
+
     const html = [
       h(
         'div',
@@ -121,7 +123,7 @@ export default {
       width: 80vw;
       max-width: 768px;
       mix-blend-mode: ${props.blend};
-      background-image: ${buffers.trim()};
+      background-image: ${buffers.join(",")};
       background-size: ${bgSizes};
       background-blend-mode: ${blendModes};
       background-position: ${bgPositions};
@@ -137,8 +139,6 @@ export default {
       ...mainBuffers(),
       ...coverBuffers(),
     ]
-    // border: /** debug */ 0.1rem solid;
-    // border-color: /** debug */ red;
     //transform: /** debug */ translateX(${props.posX});
     return html
   },
