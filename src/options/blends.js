6edac76073c07d1f87e4e4432ddd96346829e344
ph4u <1057021+ph4un00b@users.noreply.github.com>
2022-11-04 20:52:45 -0600

    refactor(Root): options

---
+++ src/options/blends.js
@@ -0,0 +1,65 @@
+export const blends = [
+    {
+      value: 'normal',
+      name: 'normal',
+    },
+    {
+      value: 'multiply',
+      name: 'multiply',
+    },
+    {
+      value: 'screen',
+      name: 'screen',
+    },
+    {
+      value: 'overlay',
+      name: 'overlay',
+    },
+    {
+      value: 'darken',
+      name: 'darken',
+    },
+    {
+      value: 'lighten',
+      name: 'lighten',
+    },
+    { value: 'color-dodge', name: 'color-dodge' },
+    {
+      value: 'color-burn',
+      name: 'color-burn',
+    },
+    {
+      value: 'hard-light',
+      name: 'hard-light',
+    },
+    {
+      value: 'soft-light',
+      name: 'soft-light',
+    },
+    {
+      value: 'difference',
+      name: 'difference',
+    },
+    {
+      value: 'exclusion',
+      name: 'exclusion',
+    },
+    {
+      value: 'hue',
+      name: 'hue',
+    },
+    {
+      value: 'saturation',
+      name: 'saturation',
+    },
+    {
+      value: 'color',
+      name: 'color',
+    },
+    { value: 'luminosity', name: 'luminosity' },
+    // "inherit",
+    // "initial",
+    // "revert",
+    // "revert-layer",
+    // "unset"
+  ]
---
+++ src/options/presets.js
@@ -0,0 +1,137 @@
+import { blends } from './blends'
+
+export const defaultPreset = {
+    version: '20221104',
+    composition: {
+      // color-dodge
+      blend: blends[6].value,
+      bright: 0.5,
+      contrast: 2.0,
+      saturate: 3.5,
+    },
+    overlay: {
+      // exclusion
+      blend: blends[11].value,
+      bright: 0.8,
+      contrast: 1.6,
+      saturate: 1.4,
+    },
+  
+    b0: {
+      zoom: 50,
+      // exclusion
+      blend: blends[11].value,
+      posX: 50,
+      posY: 50,
+      angle: 0,
+    },
+    b1: {
+      zoomW: 200,
+      zoomH: 700,
+      // hue
+      blend: blends[12].value,
+      posX: 50,
+      posY: 50,
+      angle: 0,
+    },
+    b2: {
+      zoom: 300,
+      // hard-light
+      blend: blends[8].value,
+      posX: 50,
+      posY: 50,
+      angle: 0,
+    },
+    b3: {
+      zoom: 200,
+      // exclusion
+      blend: blends[11].value,
+      posX: 50,
+      posY: 50,
+      angle: 0,
+    },
+    b20: {
+      // pos="50% 50%" zoom="50%"
+      zoom: 50,
+      // exclusion
+      blend: blends[11].value,
+      posX: 50,
+      posY: 50,
+      angle: 0,
+    },
+    b21: {
+      // pos="0% 50%" zoom="200% 400%"
+      zoomW: 200,
+      zoomH: 400,
+      // hue
+      blend: blends[12].value,
+  
+      posX: 0,
+      posY: 50,
+      angle: 0,
+    },
+    b22: {
+      // pos="-50% -50%" zoom="200%"
+      zoom: 200,
+      // hard-light
+      blend: blends[8].value,
+      posX: 50,
+      posY: 0,
+      angle: 0,
+    },
+    b23: {
+      // pos="50% 50%" zoom="200%"
+      zoom: 200,
+      // exclusion
+      blend: blends[11].value,
+      posX: 50,
+      posY: 50,
+      angle: 0,
+    },
+    animate: !false,
+    animationVelocity: 0.015,
+    showPhoto: true,
+    photo: 'https://imgix.cosmicjs.com/9d2dbb80-45c4-11ed-8ad7-d16a9b880dcb-Einmusik-Mia.jpg',
+    noiseLayer: '/noise.webp',
+    // noiseLayer: "/tool.jpg"
+    showDebug: !true,
+    background: '#171717',
+    showShadow: false,
+    boxShadow: {
+      offsetX: 27,
+      offsetY: 27,
+      blurRadius: 75,
+      spreadRadius: 2,
+      color: '#399e48',
+    },
+    content: {
+      display: !true,
+      text: 'Hola!',
+      left: 33,
+      top: 66,
+      color: '#fff',
+      size: 1,
+      weight: 100,
+      w: 0,
+      h: 0,
+      extra: '',
+      debug: true,
+    },
+    transform: !true,
+    effects: false,
+    frag: {
+      velocity: 0.25,
+      textures: [
+        { name: 'none', value: '' },
+        { name: 'copyof', value: 'copyof.jpg' },
+        { name: 'spiral', value: 'spiral.jpg' },
+        { name: 'tool', value: 'tool.jpg' },
+      ],
+      texture: '',
+      center: {
+        x: 0.5,
+        y: -1.0,
+      },
+    },
+  }
+  
